6f1fabee87ed3eb54b038b639d3c061b92b62706
Md Zahid Hussain <174180646+mdzahidh009@users.noreply.github.com>
2025-05-15 03:42:05 +0530

    Update application title and branding; modify favicon and logo images

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -11,8 +11,8 @@ const Login = () => {
         backgroundSize: "cover",
       }}>
       <div className="flex flex-col items-center mt-10 space-y-32">
-        <h1 className="text-3xl font-semibold bg-black text-white w-full text-center py-4 bg-opacity-75 rounded-2xl">
-          ABC Institute of Technology
+        <h1 className="text-5xl font-bold bg-cyan-400 text-white w-full text-center py-4 bg-opacity-75 rounded-2xl">
+          MZH Institute of Technology
         </h1>
         <div className="grid grid-cols-2 gap-28">
           <div className="h-96 w-96 space-y-11 shadow-2xl flex flex-col justify-center items-center bg-transparent backdrop-blur-md bg-[#5a51d6] bg-opacity-60 rounded-xl">
